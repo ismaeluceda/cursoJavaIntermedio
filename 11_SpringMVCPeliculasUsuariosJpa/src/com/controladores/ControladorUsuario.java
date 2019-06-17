84ae29a3626218b61829b8a31bfd11156c49c9d9
IUM <ismaeluceda@gruposantander.com>
2019-06-17 09:23:11 +0200

    .

---
+++ 11_SpringMVCPeliculasUsuariosJpa/src/com/controladores/ControladorUsuario.java
@@ -0,0 +1,49 @@
+package com.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.modelo.entidades.Usuario;
+import com.modelo.negocio.GestorUsuario;
+
+@Controller
+public class ControladorUsuario {
+
+	@Autowired
+	private GestorUsuario gu;
+	
+	@Autowired
+	private Usuario usuario;
+	
+	//Solo entraremos por aqui cuando la url sea validar
+	//y el verbo http sea POST
+	@RequestMapping(value="validar",method=RequestMethod.POST)
+	public String validar(
+			@RequestParam("nombre") String nombre,
+			@RequestParam("password") String password,
+			Model model){
+		
+		usuario.setNombre(nombre);
+		usuario.setPassword(password);
+		boolean estaValidado = gu.validarUsuario(usuario);
+		if(estaValidado) {
+			return "redirect:/mvc/peliculas/verListado";
+		}else {
+			String error = "Usuario con nombre: " + nombre +
+					" y password " + password + " no existe";
+			model.addAttribute("error",error);
+			return "redirect:/index.jsp";
+		}
+	}
+}
+
+
+
+
+
+
+
